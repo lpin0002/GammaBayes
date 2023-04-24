e0a13a1a613e3ce72347ddc3fdfad38c353e71c1
lpin0002 <workpinchy@gmail.com>
2023-04-24 18:26:55 +1000

    Adding first ozstar segmentation runs

---
+++ data/ozstartest3/SRM44.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0_44_0.5_3
+#SBATCH --output=data/LatestFolder/DM1.0_44_0.5_1000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=1:20:00
+#SBATCH --mem-per-cpu=200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py ozstartest3 44 20 1.0 0.5 10 41 161
