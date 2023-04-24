94623e5cdf38582ce558f9636ccde920967bd512
lpin0002 <workpinchy@gmail.com>
2023-04-25 08:40:31 +1000

    Adding windowbigtest3 and 4

---
+++ data/windowbigtest3/SRM6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-1.3_6_0.5_4
+#SBATCH --output=data/LatestFolder/DM-1.3_6_0.5_10000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=12:10:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py windowbigtest3 6 400 -1.3 0.5 10 41 161 10000
---
+++ data/windowbigtest4/SRM24.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0_24_0.8_4
+#SBATCH --output=data/LatestFolder/DM1.0_24_0.8_10000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=12:10:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py windowbigtest4 24 400 1.0 0.8 10 41 161 10000
