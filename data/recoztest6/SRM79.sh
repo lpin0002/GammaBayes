d2d5b694e3ae688430d33e64b22039aa00a0bd01
lpin0002 <workpinchy@gmail.com>
2023-04-28 09:45:25 +1000

    Adding recoztests 6 and 7

---
+++ data/recoztest6/SRM79.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-1.0_79_0.8_3
+#SBATCH --output=data/LatestFolder/DM-1.0_79_0.8_1000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=1:30:00
+#SBATCH --mem-per-cpu=400
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py recoztest6 79 10 -1.0 0.8 10
