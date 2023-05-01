4b11825b9fdd1431beb2e26ec5136201e391cef3
lpin0002 <workpinchy@gmail.com>
2023-05-01 10:55:33 +1000

    Adding all of fineoztest1

---
+++ data/fineoztest1/SRM234.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0|0.5|234|3
+#SBATCH --output=data/LatestFolder/DM1.0_0.5_234_5000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=1:30:00
+#SBATCH --mem-per-cpu=800
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py fineoztest1 234 20 1.0 0.5 20
