f837aa9f53f1004ac9a4f545ade8f0699f1c0412
lpin0002 <workpinchy@gmail.com>
2023-05-04 09:01:21 +1000

    Adding accuracy tests

---
+++ data/acctestgrid2/SRM9.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0|0.5|9|2
+#SBATCH --output=data/LatestFolder/DM1.0_0.5_9_100.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=800
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py acctestgrid2 9 10 1.0 0.5 20
---
+++ data/acctestgrid9/SRM3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-1.0|0.2|3|2
+#SBATCH --output=data/LatestFolder/DM-1.0_0.2_3_100.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=800
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py acctestgrid9 3 10 -1.0 0.2 20
