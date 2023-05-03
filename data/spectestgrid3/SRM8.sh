392b45a986942eeb97ce4cb752625ee87b9d3c1d
lpin0002 <workpinchy@gmail.com>
2023-05-03 12:03:28 +1000

    Adding first three grid points of spectra tests

---
+++ data/spectestgrid3/SRM8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0|0.2|8|2
+#SBATCH --output=data/LatestFolder/DM1.0_0.2_8_100.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=800
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py spectestgrid3 8 10 1.0 0.2 20
