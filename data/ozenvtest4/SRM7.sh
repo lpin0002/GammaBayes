c0630de60e5a66af383ac3f69fa4ddf96d400b21
lpin0002 <workpinchy@gmail.com>
2023-05-05 12:53:57 +1000

    Adding ozenvtest4

---
+++ data/ozenvtest4/SRM7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.0|0.8|7|2
+#SBATCH --output=data/LatestFolder/DM0.0_0.8_7_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0:20:00
+#SBATCH --mem-per-cpu=200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py ozenvtest4 7 20 0.0 0.8 20
