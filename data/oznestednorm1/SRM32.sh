732cf0086866511690a7bc95c7a09fe25575c566
lpin0002 <workpinchy@gmail.com>
2023-05-11 12:56:09 +1000

    Adding oznestednorm1

---
+++ data/oznestednorm1/SRM32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.0|0.5|32|2
+#SBATCH --output=data/LatestFolder/DM0.0_0.5_32_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=500
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py oznestednorm1 32 10 0.0 0.5 10
