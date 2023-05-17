ee2ac045d29cd0cf3080ed936cffe7eb460aecad
lpin0002 <workpinchy@gmail.com>
2023-05-17 18:49:12 +1000

    Adding nostop 16, 17, 18, 19, and 20

---
+++ data/nostop18/SRM5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.5|0.5|5|2
+#SBATCH --output=data/LatestFolder/DM0.5_0.5_5_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2:50:00
+#SBATCH --mem-per-cpu=900
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py nostop18 5 100 0.5 0.5 10
