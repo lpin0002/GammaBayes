fabcf8a109d98c36c8188d6e3b2a327be4fb90a5
lpin0002 <workpinchy@gmail.com>
2023-05-17 18:03:25 +1000

    Adding nostop 7, 8, 9, and 10

---
+++ data/nostop10/SRM5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.0|0.8|5|2
+#SBATCH --output=data/LatestFolder/DM0.0_0.8_5_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2:50:00
+#SBATCH --mem-per-cpu=900
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py nostop10 5 100 0.0 0.8 10
---
+++ data/nostop9/SRM2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.5|0.8|2|2
+#SBATCH --output=data/LatestFolder/DM-0.5_0.8_2_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2:50:00
+#SBATCH --mem-per-cpu=900
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py nostop9 2 100 -0.5 0.8 10
