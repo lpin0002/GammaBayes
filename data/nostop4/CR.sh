b6c2aa35504e7beba2f9cafdd697b24c86264117
lpin0002 <workpinchy@gmail.com>
2023-05-17 17:28:31 +1000

    Adding nostop4, nostop5 and nostop6

---
+++ data/nostop4/CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR0.5|1.0|2
+#SBATCH --output=data/LatestFolder/CR0.5_1.0_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:30:00
+#SBATCH --mem-per-cpu=1600
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 recycling.py nostop4 32
---
+++ data/nostop5/SRM3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM1.0|1.0|3|2
+#SBATCH --output=data/LatestFolder/DM1.0_1.0_3_500.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2:50:00
+#SBATCH --mem-per-cpu=900
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py nostop5 3 100 1.0 1.0 10
