caeb857864913b106adc2519e56750616bdb45b2
lpin0002 <workpinchy@gmail.com>
2023-05-18 09:43:22 +1000

    Adding longer tests 1, 2, and 3

---
+++ data/longertest2/SRM4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.5|1.0|4|3
+#SBATCH --output=data/LatestFolder/DM-0.5_1.0_4_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2:50:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py longertest2 4 200 -0.5 1.0 10
