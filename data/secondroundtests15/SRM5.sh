6f10a30179fd600a6f7d928c9e2ed5d7f5e65bd8
lpin0002 <workpinchy@gmail.com>
2023-05-24 08:35:52 +1000

    Adding secondroundtests 10 and 15

---
+++ data/secondroundtests15/SRM5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.0|0.5|5|3
+#SBATCH --output=data/LatestFolder/DM0.0_0.5_5_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=5:30:00
+#SBATCH --mem-per-cpu=1600
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py secondroundtests15 5 200 0.0 0.5 10
