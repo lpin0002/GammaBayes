23e0f67ed5a59bb4d6900016e4ebdf2956264cb4
lpin0002 <workpinchy@gmail.com>
2023-05-24 15:17:47 +1000

    Adding secondroundtests 6,7,8,9

---
+++ data/secondroundtests8/CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR-0.5|0.8|3
+#SBATCH --output=data/LatestFolder/CR-0.5_0.8_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=48:30:00
+#SBATCH --mem-per-cpu=1700
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 recycling.py secondroundtests8 32
---
+++ data/secondroundtests8/SRM3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.5|0.8|3|3
+#SBATCH --output=data/LatestFolder/DM-0.5_0.8_3_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=5:30:00
+#SBATCH --mem-per-cpu=1600
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py secondroundtests8 3 200 -0.5 0.8 10
