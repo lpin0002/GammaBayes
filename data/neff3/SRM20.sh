9ff008d82276ba9e402a6a8b71d3c76284a9ba0d
lpin0002 <workpinchy@gmail.com>
2023-05-27 09:56:44 +1000

    Adding neff1, neff2, neff3

---
+++ data/neff3/SRM20.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.5|0.8|20|3
+#SBATCH --output=data/LatestFolder/DM-0.5_0.8_20_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py neff3 20 50 -0.5 0.8 20
