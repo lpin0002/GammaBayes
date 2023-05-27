b97495c67bd4f7a4b22730bf0ce6a85813655a8c
lpin0002 <workpinchy@gmail.com>
2023-05-27 10:00:47 +1000

    Adding neff4, neff5, neff6

---
+++ data/neff6/SRM21.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.25|0.8|21|3
+#SBATCH --output=data/LatestFolder/DM0.25_0.8_21_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py neff6 21 50 0.25 0.8 20
---
+++ data/neff6/SRM4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM0.25|0.8|4|3
+#SBATCH --output=data/LatestFolder/DM0.25_0.8_4_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py neff6 4 50 0.25 0.8 20
