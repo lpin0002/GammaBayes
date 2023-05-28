787f00a224c1c76d03c3955cab0c8c52c41163ca
lpin0002 <workpinchy@gmail.com>
2023-05-28 10:14:20 +1000

    Adding secondneff1,2,3

---
+++ data/secondneff2/SRM17.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.5|0.8|17|3
+#SBATCH --output=data/LatestFolder/DM-0.5_0.8_17_2000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py secondneff2 17 50 -0.5 0.8 20
