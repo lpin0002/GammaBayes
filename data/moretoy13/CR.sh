bc80577f3debfc27ea33b9d754ff7c86d686a435
lpin0002 <workpinchy@gmail.com>
2023-05-30 19:01:34 +1000

    Adding moretoy 10,11,12,13

---
+++ data/moretoy13/CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR1.5|0.8|3
+#SBATCH --output=data/LatestFolder/CR1.5_0.8_1000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 recycling.py moretoy13 32
