e0d74235a8c89d3924974936855d18d6e2f898a6
lpin0002 <workpinchy@gmail.com>
2023-05-31 14:30:09 +1000

    Adding first four full runs of erictest

---
+++ data/erictest3/SRM4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=DM-0.75|0.5|4|2
+#SBATCH --output=data/LatestFolder/DM-0.75_0.5_4_400.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=1:30:00
+#SBATCH --mem-per-cpu=1200
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 marginalisationnested.py erictest3 4 100 -0.75 0.5 20
