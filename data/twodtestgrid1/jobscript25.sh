389ae820e01fa0afb5e12622a8f390e636830ea4
lpin0002 <workpinchy@gmail.com>
2023-06-26 14:08:38 +1000

    adding twodtestgrid1

---
+++ data/twodtestgrid1/jobscript25.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR-0.8|0.8|25|4
+#SBATCH --output=data/LatestFolder/SR-0.8_0.8_25_10000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:30:00
+#SBATCH --mem-per-cpu=300
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py twodtestgrid1 25 400 -0.8 0.8
