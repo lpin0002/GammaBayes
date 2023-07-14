8248ff84417d2329ed081f67bfef233c104db23f
lpin0002 <workpinchy@gmail.com>
2023-07-14 15:14:50 +1000

    Adding crude pp tests with direct pipeline

---
+++ data/ozstarcheck2/jobscript1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.0|0.5|1|3
+#SBATCH --output=data/LatestFolder/SR0.0_0.5_1_1000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=500
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py ozstarcheck2 1 1000 0.0 0.5
