fb3631731f380905fa182297887d8c7e63632072
lpin0002 <workpinchy@gmail.com>
2023-07-24 09:08:09 +1000

    Adding e5 tests 6, 7, and 8

---
+++ data/ozstar_e5_test8/CR.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR0.0|0.5|5
+#SBATCH --output=data/LatestFolder/CR0.0_0.5_100000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:30:00
+#SBATCH --mem-per-cpu=420000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 calculateirfvalues.py ozstar_e5_test8 1
+srun python3 gridsearch.py ozstar_e5_test8 51 51 1
