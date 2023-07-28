0affeaf319cecf124a4a47ef0e03f6068d052421
lpin0002 <workpinchy@gmail.com>
2023-07-28 14:14:58 +1000

    Adding complete 3d initial pp tests

---
+++ data/ozstar_pp_check_1e4_2/CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR0.0|0.5|4
+#SBATCH --output=data/LatestFolder/CR0.0_0.5_10000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:20:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 gridsearch.py ozstar_pp_check_1e4_2
