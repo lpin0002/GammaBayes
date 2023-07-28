b59f541ed759a464b7ce6e1f393c9ac5a7099575
lpin0002 <workpinchy@gmail.com>
2023-07-28 14:44:55 +1000

    Adding follow up pp tests

---
+++ data/pp_check2_1/jobscript1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.0|0.5|1|4
+#SBATCH --output=data/LatestFolder/SR0.0_0.5_1_10000.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=0:50:00
+#SBATCH --mem-per-cpu=2100
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py pp_check2_1 1 10000 0.0 0.5
+srun python3 calculateirfvalues.py pp_check2_1 1 1 101 161 32
