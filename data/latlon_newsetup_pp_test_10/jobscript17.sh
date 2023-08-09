e28e84aeaf352e8da47216ebf39786ecdf130771
lpin0002 <workpinchy@gmail.com>
2023-08-09 21:34:39 +1000

    Adding latlon_newsetup_pp_test_10 and _11

---
+++ data/latlon_newsetup_pp_test_10/jobscript17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.5|0.5|17|5|latlon_newsetup_pp_test_10
+#SBATCH --output=data/LatestFolder/SR0.5_0.5_17_100000_latlon_newsetup_pp_test_10.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=3:30:00
+#SBATCH --mem-per-cpu=16000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py latlon_newsetup_pp_test_10 17 5000 0.5 0.5
+srun python3 nuisancemarginalisation.py latlon_newsetup_pp_test_10 17 20 61 8
