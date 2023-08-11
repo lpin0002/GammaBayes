f0c13be91090cfc476e60d297cba6d600a168728
lpin0002 <workpinchy@gmail.com>
2023-08-11 10:09:59 +1000

    Adding big_pp_test_2

---
+++ data/big_pp_test_2/jobscript48.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.5|0.5|48|6|big_pp_test_2
+#SBATCH --output=data/LatestFolder/SR0.5_0.5_48_1000000_big_pp_test_2.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=8:30:00
+#SBATCH --mem-per-cpu=16500
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py big_pp_test_2 48 20000 0.5 0.5
+srun python3 nuisancemarginalisation.py big_pp_test_2 48 50 61 8
