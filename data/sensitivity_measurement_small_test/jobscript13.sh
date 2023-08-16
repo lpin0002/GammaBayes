005823e93d749d3418b0732cf766d1b6bd5ac23c
lpin0002 <workpinchy@gmail.com>
2023-08-16 12:03:08 +1000

    Adding sensitivity_measurement_small_test

---
+++ data/sensitivity_measurement_small_test/jobscript13.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.5|0.0|13|5|sensitivity_measurement_small_test
+#SBATCH --output=data/LatestFolder/SR0.5_0.0_13_100000_sensitivity_measurement_small_test.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=7
+#SBATCH --time=10:30:00
+#SBATCH --mem-per-cpu=17000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py sensitivity_measurement_small_test 13 2000 0.5 0.0
+srun python3 nuisancemarginalisation.py sensitivity_measurement_small_test 13 50 51 7
