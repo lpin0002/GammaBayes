684da97b3c77dadae56c403a9ebceafd07240a86
lpin0002 <workpinchy@gmail.com>
2023-08-22 21:06:21 +1000

    Adding ozs_newbkg

---
+++ data/ozs_newbkg/jobscript2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.0|0.5|2|4|ozs_newbkg
+#SBATCH --output=data/LatestFolder/SR0.0_0.5_2_20000_ozs_newbkg.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=1:10:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py ozs_newbkg 2 10000 0.0 0.5
+srun python3 nuisancemarginalisation.py ozs_newbkg 2 2 51 16
