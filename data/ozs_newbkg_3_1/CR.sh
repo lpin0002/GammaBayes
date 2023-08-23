00dd2865d2b4a77a0573f2d4a4f0a8a0cb3598bb
lpin0002 <workpinchy@gmail.com>
2023-08-23 13:48:02 +1000

    Adding ozs_newbkg_3 extra runs 1 & 2

---
+++ data/ozs_newbkg_3_1/CR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=CR0.0|0.02|6|ozs_newbkg_3_1
+#SBATCH --output=data/LatestFolder/CR0.0_0.02_1000000_ozs_newbkg_3_1.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:30:00
+#SBATCH --mem-per-cpu=10000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 gridsearch.py ozs_newbkg_3_1 161 1
