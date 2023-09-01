d00f53149c57b9170c911aa37de947e385a9cb5b
lpin0002 <workpinchy@gmail.com>
2023-09-01 10:01:54 +1000

    Adding re_ex_eres_200_test4

---
+++ data/re_ex_eres_200_test4/jobscript1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=SR0.0|0.5|1|4|re_ex_eres_200_test4
+#SBATCH --output=data/LatestFolder/SR0.0_0.5_1_20000_re_ex_eres_200_test4.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=16:50:00
+#SBATCH --mem-per-cpu=2400
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 simulation.py re_ex_eres_200_test4 1 10000 0.0 0.5
+srun python3 nuisancemarginalisation.py re_ex_eres_200_test4 1 2 51 24
