7924b21909688b7fdd13e47c9d199468ebad1887
lpin0002 <workpinchy@gmail.com>
2023-09-23 12:12:26 +1000

    Name change to main folder

---
+++ gammabayes/ozstarsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=PipelineSetup
+#SBATCH --output=data/LatestFolder/SetupOutput.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=32000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=progressemail1999@gmail.com
+source activate DMPipe
+srun python3 gammabayes/default_file_setup.py
