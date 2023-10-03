7697c678b9b7ac290e177044a113b1dce52baef7
lpin0002 <workpinchy@gmail.com>
2023-10-03 22:28:15 +1100

    class if instead of elif

--- gammabayes/hyperparameter_likelihood.py
+++ gammabayes/hyperparameter_likelihood.py
@@ -28,7 +28,7 @@ class hyperparameter_likelihood(object):
         self.marg_results               = marg_results
         if mixture_axes is None:
             self.mixture_axes               = np.array([np.linspace(0,1,101)]*len(priors))
-        if len(mixture_axes) != len(priors):
+        elif len(mixture_axes) != len(priors):
             self.mixture_axes               = np.array([mixture_axes]*len(priors))
         else:
             self.mixture_axes = np.array([*mixture_axes])
