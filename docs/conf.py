7ea004d87a5998dc59aed69b9537c087c5583c04
lpin0002 <workpinchy@gmail.com>
2023-10-19 10:24:36 +1100

    Changes to upload docs to readthedocs using sphinx

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'GammaBayes'
+copyright = '2023, Liam Pinchbeck'
+author = 'Liam Pinchbeck'
+release = '0.0.42'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+]
+
+master_doc = 'index'
+
+nbsphinx_execute = 'never'  # Set to 'never' if you want to use static notebooks
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+source_suffix = '.rst'
+
+
+
+nbsphinx_prolog = """
+.. role:: raw-html(raw)
+   :format: html
+
+.. nbinfo::
+
+   This page is a tutorial for the <a href="https://github.com/lpin0002/GammaBayes">GammaBayes</a> 
+   and the original notebook can be found within the docs/tutorials folder within the GitHub repository.
+
+"""
---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. GammaBayes documentation master file, created by
+   sphinx-quickstart on Thu Oct 19 10:08:01 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to GammaBayes's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   tutorials/framework_introduction
+   tutorials/astrophysical_background
+   tutorials/Dark_matter_models
+   tutorials/prior_class
+   tutorials/likelihood_class
+   tutorials/Simulations
+   tutorials/hyperparameter_inference
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
