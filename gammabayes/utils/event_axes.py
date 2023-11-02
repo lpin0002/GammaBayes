5fa773985f935c5e4114a76b0b7eda3ad1464449
lpin0002 <workpinchy@gmail.com>
2023-11-02 11:26:05 +1100

    Increasing true energy resolution

--- gammabayes/utils/event_axes.py
+++ gammabayes/utils/event_axes.py
@@ -22,7 +22,7 @@ log10estart             = -1
 log10eend               = 2
 log10erange             = log10eend - log10estart
 log10eaxis              = np.linspace(log10estart,log10eend,int(np.round(log10erange*50))+1)
-log10eaxistrue          = np.linspace(log10estart,log10eend,int(np.round(log10erange*200))+1)
+log10eaxistrue          = np.linspace(log10estart,log10eend,int(np.round(log10erange*300))+1)
 
 
 
