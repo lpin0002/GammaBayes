b75dff03b1d0c5915407da36c4cbe3f418bde6e9
lpin0002 <workpinchy@gmail.com>
2024-01-03 16:55:33 +1100

    Making an exception case more informative

--- gammabayes/likelihoods/irfs/prod5/__init__.py
+++ gammabayes/likelihoods/irfs/prod5/__init__.py
@@ -4,4 +4,4 @@ try:
     from .load_irf_norms import log_psf_normalisations, log_edisp_normalisations
 except Exception as e:
     # Print the error message without raising the exception
-    print(f"An error occurred: {e}")
+    print(f"We caught an error, but will ignore it: {e}")
