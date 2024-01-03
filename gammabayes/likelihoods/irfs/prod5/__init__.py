655fffc84bb2889a8e62543c5c5bb8a03ca49999
lpin0002 <workpinchy@gmail.com>
2024-01-03 17:20:07 +1100

    Removing standard normalisation import

--- gammabayes/likelihoods/irfs/prod5/__init__.py
+++ gammabayes/likelihoods/irfs/prod5/__init__.py
@@ -1,7 +1,7 @@
 from .gammapy_wrappers import *
 import warnings
-try:
-    from .load_irf_norms import log_psf_normalisations, log_edisp_normalisations
-except Exception as e:
-    # Print the error message without raising the exception
-    print(f"We caught an error, but will ignore it: {e}")
+# try:
+#     from .load_irf_norms import log_psf_normalisations, log_edisp_normalisations
+# except Exception as e:
+#     # Print the error message without raising the exception
+#     print(f"We caught an error, but will ignore it: {e}")
