4ac05b0f535a0321f154d70832785f269e66576e
lpin0002 <workpinchy@gmail.com>
2024-03-10 09:37:32 +1100

    Updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 source_suffix = '.rst'
 
