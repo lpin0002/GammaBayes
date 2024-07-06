Fixing some reamining issues relating to astropy units

gammabayes/high_level_inference/general_mixture/setup.py
@@ -283,6 +283,8 @@ EDISP: {self.log_edisp_norm_matrix_path}\n\n\n""")
         else:
             self.DM_Annihilation_Ratios = None
 
+        if not('marginalisation_bounds' in self.config_dict):
+            self.config_dict['marginalisation_bounds'] = None
 
 
 
@@ -296,6 +298,8 @@ EDISP: {self.log_edisp_norm_matrix_path}\n\n\n""")
 
 
         for model_idx, model_name in enumerate(self.observational_prior_model_names):
+
+            print(f"Setting up {model_name}")
             
             if not(model_name in ['DM', 'CCR_BKG', 'FG_HS_CCR']):
 
@@ -339,7 +343,7 @@ EDISP: {self.log_edisp_norm_matrix_path}\n\n\n""")
                 log_CCR_matrix = self.irf_loglike.log_bkg_CCR(energy=true_meshes[0], longitude=true_meshes[1], latitude=true_meshes[2])
 
 
-                log_bkg_matrix = special.logsumexp([np.log(fermi_gaggero_rate_matrix), np.log(hess_source_rate_matrix), log_CCR_matrix], axis=0)
+                log_bkg_matrix = special.logsumexp([np.log(fermi_gaggero_rate_matrix.value), np.log(hess_source_rate_matrix.value), log_CCR_matrix], axis=0)
 
                 # Pseudo-normalisation to regularise/make calculations more numerically stable
                 log_bkg_matrix = log_bkg_matrix - special.logsumexp(log_bkg_matrix)

gammabayes/hyper_inference/scan_nuisance_methods/discrete_adaptive_scan_hyperparameter_likelihood.py
@@ -158,22 +158,27 @@ class DiscreteAdaptiveScan(DiscreteBruteScan):
 
 
         meshvalues  = np.meshgrid(*event_vals, *temp_axes, indexing='ij')
-        index_meshes = np.ix_( *[temp_axis_info[1] for temp_axis_info in temp_axes_and_indices])
 
+        index_meshes = np.ix_( *[temp_axis_info[1] for temp_axis_info in temp_axes_and_indices])
 
         flattened_meshvalues = [meshmatrix.flatten()*unit for meshmatrix, unit in zip(meshvalues, unit_list)]
         
         log_likelihoodvalues = np.squeeze(self.log_likelihood(*flattened_meshvalues).reshape(meshvalues[0].shape))
 
         log_likelihoodvalues = log_likelihoodvalues - self.log_likelihoodnormalisation[*index_meshes]
-        
+
+        try:
+            for idx, axis in enumerate(temp_axes):
+                temp_axes[idx] = axis.value
+        except:
+            pass
+
         all_log_marg_results = []
 
         for log_prior_matrices in log_prior_matrix_list:
             # Transpose is because of the convention for where I place the 
                 # axes of the true values (i.e. first three indices of the prior matrices)
                 # and numpy doesn't support this kind of matrix addition nicely
-            
             logintegrandvalues = (
                 np.squeeze(log_prior_matrices).T[
                     ...,
@@ -181,8 +186,7 @@ class DiscreteAdaptiveScan(DiscreteBruteScan):
                     index_meshes[1].T,
                     index_meshes[0].T
                 ] + np.squeeze(log_likelihoodvalues).T).T
-            
-            
+                        
             single_parameter_log_margvals = iterate_logspace_integration(logintegrandvalues,   
                 axes=temp_axes, axisindices=[0,1,2])
 

gammabayes/priors/astro_sources/diffuse_emission/fermi_gaggero_diffuse.py
@@ -19,7 +19,8 @@ from astropy import units as u
 
 from gammabayes.priors.core import DiscreteLogPrior
 
-fermi_rate_units = 1/u.TeV/u.s/u.sr/(u.cm**2)
+fermi_rate_units = 1/u.TeV/u.s/u.deg**2/(u.cm**2)
+fermi_obs_rate_units = 1/u.TeV/u.s/u.deg**2
 
 
 def construct_fermi_gaggero_matrix(energy_axis: np.ndarray, longitudeaxis: np.ndarray, latitudeaxis: np.ndarray,
@@ -39,7 +40,7 @@ def construct_fermi_gaggero_matrix(energy_axis: np.ndarray, longitudeaxis: np.nd
 
     energy_axis_true = MapAxis.from_nodes(energy_axis, interp='log', name="energy_true")
 
-    HESSgeom = WcsGeom.create(
+    geom = WcsGeom.create(
         skydir=SkyCoord(0, 0, unit="deg", frame='galactic'),
         binsz=(np.diff(longitudeaxis)[0], np.diff(latitudeaxis)[0]),
         width=(np.ptp(longitudeaxis)+np.diff(longitudeaxis)[0], np.ptp(latitudeaxis)+np.diff(latitudeaxis)[0]),
@@ -59,9 +60,7 @@ def construct_fermi_gaggero_matrix(energy_axis: np.ndarray, longitudeaxis: np.nd
     )
 
     # Need to flip as convention goes positive to negative 
-    fermievaluated = np.flip(np.transpose(diffuse_iem.evaluate_geom(HESSgeom), axes=(0,2,1)), axis=1).to(fermi_rate_units)
-
-    fermievaluated = (fermievaluated/energy_axis.unit)*energy_axis.unit
+    fermievaluated = np.flip(np.transpose(diffuse_iem.evaluate_geom(geom).to(fermi_rate_units), axes=(0,2,1)), axis=1)
 
     # Normalising so I can apply the normalisation of that in Gaggero et al.
     fermi_integral_values= logspace_integrator(logy=np.log(fermievaluated.value), x=energy_axis, axis=0)
@@ -70,7 +69,7 @@ def construct_fermi_gaggero_matrix(energy_axis: np.ndarray, longitudeaxis: np.nd
             logy=fermi_integral_values, x=longitudeaxis, axis=0), x=latitudeaxis, axis=0)
 
     # Slight change in normalisation due to the use of m^2 not cm^2 so there is a 10^4 change in the normalisation
-    fermi_gaggero = np.exp(fermi_integral_values+np.log(power_law(energy_axis, index=-2.41, phi0=1.36*1e-8).value)[:, np.newaxis, np.newaxis])
+    fermi_gaggero = np.exp(fermi_integral_values+np.log(power_law(energy_axis.to(u.TeV).value, index=-2.41, phi0=1.36*1e-8*u.Unit("TeV-1 cm-2 s-1 sr-1")).to(fermi_rate_units).value)[:, np.newaxis, np.newaxis])
 
     energymesh, lonmesh, latmesh = np.meshgrid(energy_axis, longitudeaxis, latitudeaxis, indexing='ij')
     
@@ -81,7 +80,7 @@ def construct_fermi_gaggero_matrix(energy_axis: np.ndarray, longitudeaxis: np.nd
 
     diffuse_background_observed_event_rate = np.exp(log_diffuse_background_source_flux+log_aeff_table)
 
-    return diffuse_background_observed_event_rate
+    return diffuse_background_observed_event_rate*fermi_obs_rate_units
 
 class construct_log_fermi_gaggero_bkg(object):
     """        
@@ -125,7 +124,7 @@ class construct_log_fermi_gaggero_bkg(object):
     
         axes = [energy_axis, longitudeaxis, latitudeaxis]
         log_fermi_diffuse = np.log(construct_fermi_gaggero_matrix(energy_axis=energy_axis, 
-        longitudeaxis=longitudeaxis, latitudeaxis=latitudeaxis, log_aeff=log_aeff))
+        longitudeaxis=longitudeaxis, latitudeaxis=latitudeaxis, log_aeff=log_aeff).to(fermi_obs_rate_units).value)
 
         if normalise:
             log_fermi_diffuse = log_fermi_diffuse - logspace_integrator(
@@ -139,7 +138,7 @@ class construct_log_fermi_gaggero_bkg(object):
         # Have to interpolate actual probabilities as otherwise these maps include -inf
         self.fermi_diffuse_interpolator = interpolate.RegularGridInterpolator(
             (*axes,), 
-            np.exp(log_fermi_diffuse)*fermi_rate_units
+            np.exp(log_fermi_diffuse)
             )
 
     # Then we make a wrapper to put the result of the function in log space
@@ -160,7 +159,7 @@ class construct_log_fermi_gaggero_bkg(object):
         return np.log(
         self.fermi_diffuse_interpolator(
             (energy, longitude, latitude)
-            ).value)
+            ))
     
 
 # Just a little helper class
@@ -226,8 +225,5 @@ class FermiGaggeroDiffusePrior(DiscreteLogPrior):
         )
         
 
-
-
-
-
+        self.unit = fermi_rate_units
 

gammabayes/priors/astro_sources/localised_sources/hess_catalogue_sources.py
@@ -19,11 +19,12 @@ from gammapy.modeling.models import (
 )
 from gammapy.catalog import SourceCatalogHGPS
 
-hess_rate_units = 1/u.TeV/u.s/u.sr/(u.cm**2)
+hess_rate_units = 1/u.TeV/u.s/u.deg**2/(u.cm**2)
+hess_obs_rate_units = 1/u.TeV/u.s/u.deg**2
 
 
 def construct_hess_source_map(energy_axis: np.ndarray, longitudeaxis: np.ndarray, latitudeaxis: np.ndarray,
-    log_aeff: callable):
+    log_aeff: callable, aeff_unit=u.cm**2):
     """Constructs a map of HESS source fluxes based on the HGPS catalog.
 
     Args:
@@ -34,6 +35,8 @@ def construct_hess_source_map(energy_axis: np.ndarray, longitudeaxis: np.ndarray
         latitudeaxis (np.ndarray): Latitude axis for the map (degrees).
         
         log_aeff (callable): Logarithm of the effective area as a function of energy, longitude, and latitude.
+
+        aeff_unit (optional): Units for effective area function
 
     Returns:
         np.ndarray: A 3D array representing the event rate from HESS catalog sources over the specified energy,
@@ -103,7 +106,7 @@ def construct_hess_source_map(energy_axis: np.ndarray, longitudeaxis: np.ndarray
                 
             count+=1
             
-    logging.info(f"The unit for the HESS map is {HESSmap.quantity.unit}")
+    print(f"The unit for the HESS map is {HESSmap.quantity.unit}")
     # Transposing the longitude and latitude values such that the order of indices goes [logenergy_index, longitude_index, latitude_index]
     full_hess_flux = np.transpose(full_hess_flux, axes=(0,2,1))
     full_hess_flux = np.flip(full_hess_flux, axis=1)
@@ -129,14 +132,15 @@ def construct_hess_source_map(energy_axis: np.ndarray, longitudeaxis: np.ndarray
 
     point_hess_background_event_rate = np.exp(log_point_hess_background_source_flux+log_aeff_table)
 
-    return point_hess_background_event_rate
+    return (point_hess_background_event_rate*HESSmap.quantity.unit*aeff_unit).to(hess_obs_rate_units)
                 
 
 
 class construct_hess_source_map_interpolation(object):
     
     def __init__(self, energy_axis: np.ndarray, longitudeaxis: np.ndarray, latitudeaxis: np.ndarray,
-                 log_aeff: callable, normalise: bool = True, iterate_logspace_integrator: callable =iterate_logspace_integration):
+                 log_aeff: callable, normalise: bool = True, iterate_logspace_integrator: callable =iterate_logspace_integration,
+                 aeff_unit = u.cm**2):
         """Provides interpolated values from the HESS source map for given energy, longitude, and latitude points.
 
         Args:
@@ -161,7 +165,7 @@ class construct_hess_source_map_interpolation(object):
 
         log_astro_sourcemap = np.log(construct_hess_source_map(energy_axis=energy_axis, 
             longitudeaxis=longitudeaxis, latitudeaxis=latitudeaxis,
-            log_aeff=log_aeff))
+            log_aeff=log_aeff, aeff_unit=aeff_unit).to(hess_obs_rate_units).value)
 
         if normalise:
             log_astro_sourcemap = log_astro_sourcemap - iterate_logspace_integrator(log_astro_sourcemap, axes=axes)
@@ -222,7 +226,9 @@ class HESSCatalogueSources_Prior(DiscreteLogPrior):
         self.log_hess_class_instance = construct_hess_source_map_interpolation(energy_axis=energy_axis, 
                                                                             longitudeaxis=longitudeaxis, 
                                                                             latitudeaxis=latitudeaxis, 
-                                                                            log_aeff=irf.log_aeff,)
+                                                                            log_aeff=irf.log_aeff,
+                                                                            aeff_unit=irf.aeff_units)
+        
         
         DiscreteLogPrior.__init__(self,
             name='HESS Catalogue Sources Prior',
@@ -231,4 +237,7 @@ class HESSCatalogueSources_Prior(DiscreteLogPrior):
             logfunction=self.log_hess_class_instance.log_func, 
             *args, **kwargs
         )
+
+        self.unit = hess_obs_rate_units
+
 