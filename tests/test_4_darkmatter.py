Fixing units in test 4

tests/test_4_darkmatter.py
@@ -15,9 +15,9 @@ from astropy import units as u
 
 
 def test_dm_spectral_cutoff():
-    energy_true_axis, longitudeaxistrue, latitudeaxistrue = np.logspace(-1,2,31)*u.TeV, np.linspace(-5,5,20), np.linspace(-4,4,16)
+    energy_true_axis, longitudeaxistrue, latitudeaxistrue = np.logspace(-1,2,31)*u.TeV, np.linspace(-5,5,20)*u.deg, np.linspace(-4,4,16)*u.deg
 
-    energy_recon_axis, longitudeaxis, latitudeaxis = np.logspace(-1,2,16)*u.TeV, np.linspace(-5,5,10), np.linspace(-4,4,8)
+    energy_recon_axis, longitudeaxis, latitudeaxis = np.logspace(-1,2,16)*u.TeV, np.linspace(-5,5,10)*u.deg, np.linspace(-4,4,8)*u.deg
 
 
     irf_loglike = IRF_LogLikelihood(axes=[energy_recon_axis, longitudeaxis, latitudeaxis], 